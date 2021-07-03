e0665d230f03e7c90588cdf984fdfe4ba8b219fe
Michael <michael.s.seitz@gmail.com>
2021-07-03 22:48:43 +0200

    aufgabe richtig gelöst

--- aufgabe.js
+++ aufgabe.js
@@ -15,8 +15,31 @@ console.log("Anzahl launches:", launches.length)
 // Aufgaben:
 // 
 // 1) Wie viele Raketen hat SpaceX im Jahr 2018 gestartet?
+function howManyLaunchesInYear(data, year){
+  let launches = 0
+  let launch_success = 0
+  let successfulPayload = 0
+  for (const element of data){
+    if (element["launch_year"] === year.toString()){
+      launches++
+      //1a)
+      if (element["launch_success"]){
+        launch_success++
+        //2)
+        for (const payload of element["rocket"]["second_stage"]["payloads"]){
+          successfulPayload+=payload["payload_mass_kg"]
+        }
+      }
+    }
+  }
+  return  "\nNumber of launches in " + year + ": " + launches + 
+          "\nSuccessful: " + launch_success + 
+          "\nUnsuccessful: " + (launches-launch_success) +//1b)
+          "\nPayload: " + successfulPayload + "kg"
+}
 //    1a) Wie viele davon erfolgreich
 //    1b) Wie viele davon nicht erfolgreich?
+console.log(howManyLaunchesInYear(launches, 2018)); // 21 wurden ausgegeben
 // 
 //    Hinweis: Schau' dir dazu die Eigenschaften "launch_year" und
 //             "launch_success" an!
